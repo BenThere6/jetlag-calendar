ca706f9703f095ccd95903c1f24e34df0a8b6186
BenThere6 <104028050+BenThere6@users.noreply.github.com>
2024-02-28 21:40:07 -0700

    add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './assets/main.css';
+import './styles/main.css';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
